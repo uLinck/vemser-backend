cca5dc0c142600bf7484226ed1f46308fb79ab18
Gustavo Linck Ferreira <gustavo.ferreira@dbccompany.com.br>
2022-10-04 13:00:33 -0300

    update: modificando texto imprimido

--- Modulo 1/conta-corrente-homework2/src/ContaPoupanca.java
+++ Modulo 1/conta-corrente-homework2/src/ContaPoupanca.java
@@ -1,9 +1,9 @@
-public class ContaPoupanca extends Conta implements impressao {
+public class ContaPoupanca extends Conta implements Impressao {
 
-     static final double JUROS_MENSAL = 1.01;
+    static final double JUROS_MENSAL = 1.01;
 
 
-     double saldoTotal;
+    double saldoTotal;
 
     public ContaPoupanca(Cliente cliente, String numeroConta, String agencia, double saldo) {
         super(cliente, numeroConta, agencia, saldo);
@@ -12,14 +12,14 @@ public class ContaPoupanca extends Conta implements impressao {
 
     public void creditarTaxa() {
 
-         saldoTotal = this.getSaldo() * JUROS_MENSAL;
+        saldoTotal = this.getSaldo() * JUROS_MENSAL;
 
-     }
+    }
 
     @Override
     public void imprimir() {
 
-        System.out.println("O novo saldo da poupança é: R$" + this.saldoTotal);
+        System.out.println("O saldo atual da conta poupança é: R$" + this.saldoTotal);
 
     }
 }
