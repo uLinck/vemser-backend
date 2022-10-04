2e40a9268d7d96012e0e73b3c7c9d4b42abc374a
Gustavo Linck Ferreira <gustavo.ferreira@dbccompany.com.br>
2022-10-04 14:10:59 -0300

    update: trocando ContaCorrente para Conta

--- Modulo 1/conta-corrente-homework2/src/Movimentacao.java
+++ Modulo 1/conta-corrente-homework2/src/Movimentacao.java
@@ -4,6 +4,6 @@ public interface Movimentacao {
 
    public boolean depositar(double valor);
 
-   public boolean transferir(ContaCorrente conta, double valor);
+   public boolean transferir(Conta conta, double valor);
 
 }
