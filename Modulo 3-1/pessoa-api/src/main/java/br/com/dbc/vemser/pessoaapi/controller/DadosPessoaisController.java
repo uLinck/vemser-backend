2c4f14a7301172b8386683888654e42e90e80a77
uLinck <linck.privado@gmail.com>
2022-10-27 00:57:52 -0300

    homework-8

---
+++ Modulo 3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/DadosPessoaisController.java
@@ -0,0 +1,48 @@
+package br.com.dbc.vemser.pessoaapi.controller;
+
+
+import br.com.dbc.vemser.pessoaapi.client.DadosPessoaisClient;
+import br.com.dbc.vemser.pessoaapi.dto.DadosPessoaisDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/dados-pessoais")
+public class DadosPessoaisController {
+
+    private final DadosPessoaisClient client;
+
+
+    @GetMapping
+    public List<DadosPessoaisDTO> getAll() {
+        return client.getAll();
+    }
+
+
+    @GetMapping("/{cpf}")
+    public DadosPessoaisDTO get(@PathVariable("cpf") String cpf) {
+        return client.get(cpf);
+    }
+
+
+    @PostMapping
+    public DadosPessoaisDTO post(@RequestBody DadosPessoaisDTO dadosPessoaisDTO) {
+        return client.post(dadosPessoaisDTO);
+    }
+
+
+    @PutMapping("/{cpf}")
+    public DadosPessoaisDTO put(@PathVariable("cpf") String cpf,
+                                @RequestBody DadosPessoaisDTO dadosPessoaisDTO) {
+        return client.put(cpf, dadosPessoaisDTO);
+    }
+
+
+    @DeleteMapping("/{cpf}")
+    public void delete(@PathVariable("cpf") String cpf) {
+        client.delete(cpf);
+    }
+}
---
+++ Modulo 3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/dto/PessoaDadosPessoaisDTO.java
@@ -0,0 +1,67 @@
+package br.com.dbc.vemser.pessoaapi.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+
+@Data
+public class PessoaDadosPessoaisDTO{
+
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "Nome da pessoa")
+    private String nome;
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "CPF da pessoa", example = "12345678901")
+    private String cpf;
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "CNH da pessoa")
+    private String cnh;
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "Nome da mãe da pessoa")
+    private String nomeMae;
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "Nome do Pai da pessoa")
+    private String nomePai;
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "RG da pessoa", example = "12345678901")
+    private String rg;
+
+    @NotNull
+    @Schema(description = "Sexo da pessoa", example = "M")
+    private Sexo sexo;
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "Titulo de eleitor", example = "12345678901")
+    private String tituloEleitor;
+
+    @NotEmpty
+    @NotBlank
+    @Schema(description = "Email da pessoa", example = "pessoa@gmail.com")
+    private String email;
+
+    @NotNull(message = "Informe a data de nascimento")
+    @Past(message = "a data deve estar no passado")
+    @Schema(description = "Data de nascimento da pessoa")
+    private LocalDate dataNascimento;
+
+}
