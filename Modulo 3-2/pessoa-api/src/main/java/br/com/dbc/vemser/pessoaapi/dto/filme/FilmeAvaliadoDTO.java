c50a1d017d735fa02c9018e9279604f93a18ba8e
uLinck <linck.privado@gmail.com>
2022-11-03 10:58:28 -0300

    tirando imports

--- Modulo 3-2/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/dto/filme/FilmeAvaliadoDTO.java
+++ Modulo 3-2/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/dto/filme/FilmeAvaliadoDTO.java
@@ -4,9 +4,7 @@ import br.com.dbc.vemser.pessoaapi.entity.DescricaoEntity;
 import br.com.dbc.vemser.pessoaapi.entity.pk.FilmePK;
 import lombok.*;
 
-import javax.persistence.Column;
 import javax.persistence.Embedded;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 @Getter
