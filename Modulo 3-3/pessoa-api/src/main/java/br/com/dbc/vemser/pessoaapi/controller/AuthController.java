13b9f785c9d9e3ca9c239e1ad775cf60dc818d5f
uLinck <linck.privado@gmail.com>
2022-11-09 00:30:19 -0300

    adicionando documentacao

--- Modulo 3-3/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/AuthController.java
+++ Modulo 3-3/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/controller/AuthController.java
@@ -5,6 +5,9 @@ import br.com.dbc.vemser.pessoaapi.entity.classes.UsuarioEntity;
 import br.com.dbc.vemser.pessoaapi.exception.RegraDeNegocioException;
 import br.com.dbc.vemser.pessoaapi.security.TokenService;
 import br.com.dbc.vemser.pessoaapi.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +27,14 @@ public class AuthController {
     private final UsuarioService usuarioService;
     private final TokenService tokenService;
 
+    @Operation(summary = "Autenticar dados", description = "Verifica se seus dados consta no banco e cria um token de acesso.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Token criado com sucesso!"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PostMapping
     public String auth(@RequestBody @Valid LoginDTO loginDTO) throws RegraDeNegocioException {
         // FIXME adicionar mecanismo de autenticação para verificar se o usuário é válido e retornar o token
