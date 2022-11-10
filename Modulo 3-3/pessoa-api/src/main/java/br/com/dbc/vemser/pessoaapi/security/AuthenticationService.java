546f184ebd406562afb8803ea47d11e3553e552a
uLinck <linck.privado@gmail.com>
2022-11-10 01:55:38 -0300

    gustavo nunca esqueca da constante gustavo, nunca esqueca

--- Modulo 3-3/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/security/AuthenticationService.java
+++ Modulo 3-3/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/security/AuthenticationService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @Service
 public class AuthenticationService implements UserDetailsService {
 
-    private UsuarioLoginService usuarioLoginService;
+    private final UsuarioLoginService usuarioLoginService;
 
     @Override
     public UserDetails loadUserByUsername(String loginUsername) throws UsernameNotFoundException {
